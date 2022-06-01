3b7291c7b0658e33688fd3acd8a0065036a30022
Saannndddyyyyy <sandhya.s2019@vitstudent.ac.in>
2022-06-01 23:27:21 +0530

    dyte assessment

---
+++ README.dockerfile
@@ -0,0 +1,17 @@
+npm install axios@0.21.1 // IN COMPAND PROMPT IN THE DIRECTORY AS PER UR WISH
+
+Create a new .js file // In my case it is dyte.js
+
+create a json file // In my case it is a package.json
+in the json filegive all keys and values needed
+
+go to command prompt navigate to ur project directory and type npm init --version
+
+create a c# file //using c# programming language instead of python hope programming language doesnt matter
+deserialize the json file in c# and store responses as csv file
+
+//my c# file dyte.csproj
+
+//THE JS FILE AND JSON FILE ARE INSIDE THE BIN FOLDER
+
+//THE C# PROJECT FILE IS INSIDE THE DYTE FOLDER 
---
+++ dyte/dyte/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+using System.IO;
+using IronXL;
+using System.Collections.Generic;
+
+namespace dyte
+{
+    class Program
+    {
+        private static string jsonString;
+
+        static void Main(string[] args)
+        {
+
+            //Creating a Workbook and adding column Names
+            WorkBook xlsWorkbook = WorkBook.Create(ExcelFileFormat.XLS);
+            xlsWorkbook.Metadata.Author = "IronXL";
+            WorkSheet xlsSheet = xlsWorkbook.CreateWorkSheet("new_sheet");
+            dynamic result = Newtonsoft.Json.JsonConvert.DeserializeObject(File.ReadAllText(@"C:\Users\sandh\dyte\package.json"));
+
+            var value = new List<string>();
+            var key= new List<string>();
+
+            foreach (var file in result)
+            {
+                value.Add(file.value); //value from package.json
+                key.Add(file); //key from package.json
+            }
+
+
+            //Writing Column Names to Woorksheet
+            xlsSheet["A1"].Value = "Name";
+            xlsSheet["B1"].Value = "Repo";
+            xlsSheet["C1"].Value = "Version";
+            
+           
+
+            //Opening Chrome Browser
+            IWebDriver wd = new ChromeDriver();
+            for (int i = 0; i < 3; i++)
+            {
+                string x = "npm init --version";
+                x = Console.ReadLine();
+                xlsSheet["C" + i].Value = x;
+                xlsSheet["B" + i].Value = value[i];
+                xlsSheet["A" + i].Value = key[i];
+
+            }
+
+             
+
+            xlsWorkbook.SaveAs(@"C:\Users\sandh\dyte\DYTE.csv");
+
+        }
+    }
+}
